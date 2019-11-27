6ab92caeae45ba23ebaf8ae06015acaf342ad7e9
José Manuel Platero <jmplatero@hotmail.com>
2019-11-27 19:35:20 +0100

    Commit inicial

--- PSP - PracticaProcesos/src/es/studium/Vista/FramePrincipal.java
+++ PSP - PracticaProcesos/src/es/studium/Vista/FramePrincipal.java
@@ -1,10 +1,23 @@
 package es.studium.Vista;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.SystemColor;
+import java.awt.Toolkit;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 
 public class FramePrincipal extends JFrame {
@@ -12,6 +25,10 @@ public class FramePrincipal extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	private JPanel contentPane;
+	private JTextField textField;
+	
+	private Toolkit herramientas = Toolkit.getDefaultToolkit();
+	private Dimension pantallaSize = herramientas.getScreenSize();
 
 	/**
 	 * Launch the application.
@@ -33,12 +50,65 @@ public class FramePrincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public FramePrincipal() {
+		
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(0, 0, (pantallaSize.width*7)/10, (pantallaSize.height*6)/10);
+		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(1, 2, 0, 0));
+		
+		JPanel pnlIzq = new JPanel();
+		contentPane.add(pnlIzq);
+		pnlIzq.setLayout(new BorderLayout(0, 0));
+		
+		JPanel pnlIzqNorte = new JPanel();
+		pnlIzq.add(pnlIzqNorte, BorderLayout.NORTH);
+		pnlIzqNorte.setLayout(new BorderLayout(0, 0));
+		
+		
+		textField = new JTextField();
+		
+		textField.setCaretColor(new Color(204, 204, 204));
+	//
+		Border line = BorderFactory.createLineBorder(new Color(204, 204, 204));
+		Border emptyTF = new EmptyBorder(0, 3, 0, 0);
+		CompoundBorder borderTF = new CompoundBorder(line, emptyTF);
+		textField.setBorder(borderTF);
+	//
+		pnlIzqNorte.add(textField, BorderLayout.CENTER);
+		textField.setColumns(10);
+		textField.setBackground(new Color(12, 12, 12));
+		textField.setForeground(new Color(204, 204, 204));
+		textField.setFont(new Font("Consolas", Font.PLAIN, 14));
+		textField.setMargin(new Insets(10,10,10,10));
+		
+		JButton btnEjecutarComando = new JButton("Ejecutar Comando");
+		pnlIzqNorte.add(btnEjecutarComando, BorderLayout.EAST);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setCaretColor(new Color(204, 204, 204));
+		textArea.setSelectionColor(SystemColor.windowBorder);
+		textArea.setSelectedTextColor(new Color(204, 204, 204));
+		Border emptyTA = new EmptyBorder(3, 3, 0, 0);
+		CompoundBorder borderTA = new CompoundBorder(line, emptyTA);
+		textArea.setBorder(borderTA);
+		textArea.setBackground(new Color(12, 12, 12));
+		textArea.setForeground(new Color(204, 204, 204));
+		textArea.setFont(new Font("Consolas", Font.PLAIN, 16));
+		pnlIzq.add(textArea, BorderLayout.CENTER);
+		
+		JPanel pnlDer = new JPanel();
+		contentPane.add(pnlDer);
+		pnlDer.setLayout(new GridLayout(2, 1, 0, 0));
+		
+		JPanel pnlDerF1C1 = new JPanel();
+		pnlDer.add(pnlDerF1C1);
+		
+		JPanel pnlDerF2C1 = new JPanel();
+		pnlDer.add(pnlDerF2C1);
 	}
 
 }
