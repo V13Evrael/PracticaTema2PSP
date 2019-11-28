6a7908d08eafda7646ae5244eb5408c16bc1889c
José Manuel Platero <jmplatero@hotmail.com>
2019-11-28 13:29:42 +0100

    Añadidos los iconos a los botones

--- PSP - PracticaProcesos/src/es/studium/Vista/FramePrincipal.java
+++ PSP - PracticaProcesos/src/es/studium/Vista/FramePrincipal.java
@@ -6,11 +6,13 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.Insets;
 import java.awt.SystemColor;
 import java.awt.Toolkit;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -29,10 +31,10 @@ import javax.swing.table.DefaultTableModel;
 public class FramePrincipal extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private JPanel contentPane;
 	private JTextField textField;
-	
+
 	private Toolkit herramientas = Toolkit.getDefaultToolkit();
 	private Dimension pantallaSize = herramientas.getScreenSize();
 	private JTable table;
@@ -57,44 +59,42 @@ public class FramePrincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public FramePrincipal() {
-		
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(0, 0, (pantallaSize.width*7)/10, (pantallaSize.height*6)/10);
+		setBounds(0, 0, (pantallaSize.width * 7) / 10, (pantallaSize.height * 6) / 10);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new GridLayout(1, 2, 0, 0));
-		
+
 		JPanel pnlIzq = new JPanel();
 		contentPane.add(pnlIzq);
 		pnlIzq.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel pnlIzqNorte = new JPanel();
 		pnlIzq.add(pnlIzqNorte, BorderLayout.NORTH);
 		pnlIzqNorte.setLayout(new BorderLayout(0, 0));
-		
-		
+
 		textField = new JTextField();
-		
+
 		textField.setCaretColor(new Color(204, 204, 204));
-	//
+		//
 		Border line = BorderFactory.createLineBorder(new Color(204, 204, 204));
 		Border emptyTF = new EmptyBorder(0, 3, 0, 0);
 		CompoundBorder borderTF = new CompoundBorder(line, emptyTF);
 		textField.setBorder(borderTF);
-	//
+		//
 		pnlIzqNorte.add(textField, BorderLayout.CENTER);
 		textField.setColumns(10);
 		textField.setBackground(new Color(12, 12, 12));
 		textField.setForeground(new Color(204, 204, 204));
 		textField.setFont(new Font("Consolas", Font.PLAIN, 14));
-		textField.setMargin(new Insets(10,10,10,10));
-		
+		textField.setMargin(new Insets(10, 10, 10, 10));
+
 		JButton btnEjecutarComando = new JButton("Ejecutar Comando");
 		pnlIzqNorte.add(btnEjecutarComando, BorderLayout.EAST);
-		
+
 		JTextArea textArea = new JTextArea();
 		textArea.setCaretColor(new Color(204, 204, 204));
 		textArea.setSelectionColor(SystemColor.windowBorder);
@@ -106,30 +106,51 @@ public class FramePrincipal extends JFrame {
 		textArea.setForeground(new Color(204, 204, 204));
 		textArea.setFont(new Font("Consolas", Font.PLAIN, 16));
 		pnlIzq.add(textArea, BorderLayout.CENTER);
-		
+
 		JPanel pnlDer = new JPanel();
 		contentPane.add(pnlDer);
 		pnlDer.setLayout(new GridLayout(2, 1, 0, 0));
-		
+
 		JPanel pnlDerF1C1 = new JPanel();
 		pnlDer.add(pnlDerF1C1);
 		pnlDerF1C1.setLayout(new GridLayout(2, 2, 0, 0));
-		
-		JButton btnPaint = new JButton("New button");
+
+		JButton btnPaint = new JButton();
+
+		ImageIcon paintIcon = new ImageIcon(FramePrincipal.class.getResource("/img/paintIcon.png"));
+		Image imgPaint = paintIcon.getImage();
+		Image newimgPaint = imgPaint.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
+		paintIcon = new ImageIcon(newimgPaint);
+		btnPaint.setIcon(paintIcon);		
 		pnlDerF1C1.add(btnPaint);
 		
-		JButton btnBloc = new JButton("New button");
+		JButton btnBloc = new JButton();
+		ImageIcon blocIcon = new ImageIcon(FramePrincipal.class.getResource("/img/notePadIcon.png"));
+		Image imgBloc = blocIcon.getImage();
+		Image newimgBloc = imgBloc.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
+		blocIcon = new ImageIcon(newimgBloc);
+		btnBloc.setIcon(blocIcon);
 		pnlDerF1C1.add(btnBloc);
-		
-		JButton btnGestion = new JButton("New button");
+
+		JButton btnGestion = new JButton();
+		ImageIcon gestionIcon = new ImageIcon(FramePrincipal.class.getResource("/img/gestionIcon.png"));
+		Image imgGestion = gestionIcon.getImage();
+		Image newimgGestion = imgGestion.getScaledInstance(110, 110, java.awt.Image.SCALE_SMOOTH);
+		gestionIcon = new ImageIcon(newimgGestion);
+		btnGestion.setIcon(gestionIcon);
 		pnlDerF1C1.add(btnGestion);
-		
-		JButton btnJuego = new JButton("New button");
+	
+		JButton btnJuego = new JButton();
+		ImageIcon juegoIcon = new ImageIcon(FramePrincipal.class.getResource("/img/juegoIcon.png"));
+		Image imgJuego = juegoIcon.getImage();
+		Image newimgJuego = imgJuego.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
+		juegoIcon = new ImageIcon(newimgJuego);
+		btnJuego.setIcon(juegoIcon);		
 		pnlDerF1C1.add(btnJuego);
-		
+
 		JPanel pnlDerF2C1 = new JPanel();
 		pnlDer.add(pnlDerF2C1);
-		
+
 		table = new JTable();
 		table.setShowHorizontalLines(false);
 		table.setShowVerticalLines(false);
@@ -137,26 +158,19 @@ public class FramePrincipal extends JFrame {
 		table.setBackground(Color.WHITE);
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null, null},
-				{null, null, null},
-				{null, null, null},
-				{null, null, null},
-			},
-			new String[] {
-				"Estado del proceso", "Nombre del proceso", "PID del proceso"
-			}
-		));
+				new Object[][] { { null, null, null }, { null, null, null }, { null, null, null },
+						{ null, null, null }, },
+				new String[] { "Estado del proceso", "Nombre del proceso", "PID del proceso" }));
 		table.getColumnModel().getColumn(1).setPreferredWidth(105);
 		table.getColumnModel().getColumn(2).setPreferredWidth(90);
 		pnlDerF2C1.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panel = new JPanel();
 		panel.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		panel.setBackground(Color.DARK_GRAY);
 		panel.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
 		pnlDerF2C1.add(panel, BorderLayout.NORTH);
-		
+
 		JLabel lblNewLabel = new JLabel("Gesti\u00F3n de procesos");
 		lblNewLabel.setForeground(Color.WHITE);
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -164,7 +178,7 @@ public class FramePrincipal extends JFrame {
 		JScrollPane scrollPane = new JScrollPane(table);
 		scrollPane.setBorder(new LineBorder(Color.BLACK, 1, true));
 		pnlDerF2C1.add(scrollPane, BorderLayout.CENTER);
-		//pnlDerF2C1.add(table, BorderLayout.CENTER);
+		// pnlDerF2C1.add(table, BorderLayout.CENTER);
 	}
 
 }
